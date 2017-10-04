07ddc27f61d99c727ebe7a3f31779d4fc8ce1f36
Carlos <cbb00@iesemilidarder.com>
2017-10-04 20:58:03 +0200

    comienzo

---
+++ .idea/src/Task31.java
@@ -0,0 +1,11 @@
+public class Task31 {
+    public static void main(String[] args){
+        int i = 0;
+
+        for (i = 0; i <=10000; i++)
+
+                System.out.println(i);
+            }
+    }
+
+
